c8b56e4281a3b2184f1394bf0953c3fe7e1e9203
littleread <johnfox4000@gmail.com>
2017-05-21 19:18:15 -0400

    added gallery page and fixed up contact page

--- public/script.js
+++ public/script.js
@@ -1,41 +1,26 @@
 $(function() {
   console.log("ready!");
   
+  // Initialize collapse button
+  $('.button-collapse').sideNav({
+       menuWidth: 150, // Default is 300
+       edge: 'right', // Choose the horizontal origin
+       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+       draggable: true // Choose whether you can drag to open on touch screens
+     }
+   );
+  
   // Video Gallery
   $(".html5gallery-title-text-0").replaceWith($(".music-title-box-more-videos"));
-
+  
   $(".html5gallery-left-0, .html5gallery-right-0").click(function() {
     $(".html5gallery-title-text-0").replaceWith($(".music-title-box-more-videos"));
   });
   
-  // $(document).scroll(function() {
-  //   var y = $(this).scrollTop();
-  //   if (y > 1) {
-  //     $('.nav-wrapper').fadeIn();
-  //   } else {
-  //     $('.nav-wrapper').fadeOut();
-  //   }
-  // 
-  // });
-  
-  // SideNav
-  // $('.button-collapse').sideNav({
-  //   menuWidth: 300, // Default is 300
-  //   edge: 'right', // Choose the horizontal origin
-  //   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  //   draggable: true // Choose whether you can drag to open on touch screens
-  // });
-  // 
-  // // Slider
-  // $('.slider').slider();
-  // // Pause slider
-  // $('.slider').slider('pause');
-  // // Start slider
-  // $('.slider').slider('start');
-  // // Next slide
-  // $('.slider').slider('next');
-  // // Previous slide
-  // $('.slider').slider('prev');
+  (function() { 
+    Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria');
+}());
   
 
 });
